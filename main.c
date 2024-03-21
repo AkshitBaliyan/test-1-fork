e52d45bb4fa7895cfd8d53f135e14fed31468c98
Akshit Baliyan <akshitbaliyan0248@gmail.com>
2024-03-21 23:23:53 +0530

    Initial commit

--- main.c
+++ main.c
@@ -14,5 +14,26 @@ int binary_search(int arr[], int left, int right, int key) {
 }
 
 int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
 
+    int arr[n];
+    printf("Enter %d integers in sorted order: ", n);
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &arr[i]);
+    }
+
+    int key;
+    printf("Enter the target integer to search for: ");
+    scanf("%d", &key);
+
+    int result = binary_search(arr, 0, n - 1, key);
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found\n");
+    }
+
+    return 0;
 }
